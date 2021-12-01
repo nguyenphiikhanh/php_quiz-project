6368c41b0f9b2b84fb28ad87c2f66c8d3ff2bc87
Nguyen Phi Khanh <nguyenphiikhanh@gmail.com>
2021-12-01 18:29:03 +0700

    done register module

--- dangky.php
+++ dangky.php
@@ -7,8 +7,14 @@
             <h1 class="text-center mt-2">Đăng ký thành viên</h1>
             <p class="card-text text-center">Điền thông tin đăng ký tài khoản truy cập vào khóa học.</p>
 
+            <div class="alert alert-success text-center" id="success" style="display: none;" role="alert">
+                <strong>Đăng ký thành công. </strong>Bạn sẽ được chuyển đến trang đăng nhập. Vui lòng đợi...
+            </div>
+            <div class="alert alert-danger text-center" id="fail" style="display: none;" role="alert">
+                <strong>Lỗi: Tài khoản đã tồn tại trên hệ thống.</strong> Vui lòng chọn tên đăng nhập khác.
+            </div>
             <div class="row mt-2 justify-content-center">
-                <form class="col-md-9" action="">
+                <form class="col-md-9" action="" method="post">
                     <div class="input-group input-group-lg mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fas fa-user-alt"></i></span>
@@ -45,12 +51,43 @@
                 </form>
             </div>
         </div>
-        
+
         <div class="col-md-6 mt-5">
             <img class="card-img" src="./img/intro_register.png">
         </div>
     </div>
 </div>
+
+<?php
+require_once('./config/connection.php');
+
+if (isset($_POST['submit'])) {
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    $link = new Connection;
+    $count = 0;
+    $query = "SELECT * FROM users WHERE username = '$_POST[username]'";
+    $res = mysqli_query($link->link, $query);
+    $count = mysqli_num_rows($res);
+    if ($count > 0) { ?>
+        <script>
+            document.getElementById("fail").style.display = "block";
+            document.getElementById("success").style.display = "none";
+        </script>
+    <?php } else {
+        $query = "INSERT INTO users VALUES(NULL,'$_POST[username]','$_POST[fullname]','$_POST[email]','$password',DEFAULT)";
+        mysqli_query($link->link, $query);
+    ?>
+        <script>
+            document.getElementById("fail").style.display = "none";
+            document.getElementById("success").style.display = "block";
+        </script>
+<?php
+        header("refresh:3;url=dangnhap.php");
+    }
+    $link->close_connect();
+}
+?>
 
 <script type="text/javascript">
     document.title = "Đăng ký thành viên"
