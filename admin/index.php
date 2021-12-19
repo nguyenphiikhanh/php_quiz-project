124d62ca19c1381b8a4bcc029788c1a51eab4047
Nguyen Phi Khanh <nguyenphiikhanh@gmail.com>
2021-12-19 20:33:17 +0700

    setting layout for admin pages

--- admin/index.php
+++ admin/index.php
@@ -23,71 +23,14 @@
 </head>
 
 <body>
-    <!-- Left Panel -->
-
-    <aside id="left-panel" class="left-panel">
-        <nav class="navbar navbar-expand-sm navbar-default">
-
-            <div class="navbar-header">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="index.php"><img src="images/admin_logo.png" alt="Logo Display"></a>
-                <a class="navbar-brand hidden" href="index.php"><img src="images/hidden-logo.png" alt="Logo Hidden"></a>
-            </div>
-
-            <div id="main-menu" class="main-menu collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="index.html"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
-                    </li>
-                    <li>
-                        <a href="index.html"> <i class="menu-icon fa fa-power-off"></i>Đăng xuất </a>
-                    </li>
-
-
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
-    </aside><!-- /#left-panel -->
-
-    <!-- Left Panel -->
+    <!-- sidenav   -->
+    <?php include_once("./layout/sidenav.php") ?>
 
     <!-- Right Panel -->
 
     <div id="right-panel" class="right-panel">
-
-        <!-- Header-->
-        <header id="header" class="header">
-
-            <div class="header-menu">
-
-                <div class="col-sm-7">
-                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
-                </div>
-
-                <div class="col-sm-5">
-                    <div class="user-area dropdown float-right">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="user-avatar rounded-circle" src="images/admin_root.jpg" alt="Admin Avatar">
-                        </a>
-
-                        <div class="user-menu dropdown-menu">
-                            <a class="nav-link" href="#"><i class="fa fa-user"></i> My Profile</a>
-
-                            <a class="nav-link" href="#"><i class="fa fa-user"></i> Notifications <span class="count">13</span></a>
-
-                            <a class="nav-link" href="#"><i class="fa fa-cog"></i> Settings</a>
-
-                            <a class="nav-link" href="#"><i class="fa fa-power-off"></i> Logout</a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-        </header><!-- /header -->
-        <!-- Header-->
+        <!-- header  -->
+        <?php include_once("./layout/header.php") ?>
 
         <div class="breadcrumbs">
             <div class="col-sm-4">
---
+++ admin/layout/sidenav.php
@@ -0,0 +1,29 @@
+    <!-- Left Panel -->
+
+    <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php"><img src="images/admin_logo.png" alt="Logo Display"></a>
+                <a class="navbar-brand hidden" href="index.php"><img src="images/hidden-logo.png" alt="Logo Hidden"></a>
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.html"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+                    <li>
+                        <a href="index.html"> <i class="menu-icon fa fa-power-off"></i>Đăng xuất </a>
+                    </li>
+
+
+                </ul>
+            </div>
+        </nav>
+    </aside>
+
+    <!-- Left Panel -->
