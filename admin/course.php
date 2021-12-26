c570305340d58988ea9314c74d603e593dd97466
Nguyen Phi Khanh <nguyenphiikhanh@gmail.com>
2021-12-26 15:17:33 +0700

    remove module create and delete course

--- admin/course.php
+++ admin/course.php
@@ -51,34 +51,6 @@
                     <div class="col-lg-12">
                         <div class="card">
                             <div class="card-body">
-                                <div class="col-lg-12 mt-2">
-                                    <div class="card">
-                                        <div class="card-header"><strong>Thêm khóa học</strong></div>
-                                        <form action="" method="post" enctype="multipart/form-data">
-                                            <div class="card-body card-block">
-                                                <div class="form-group">
-                                                    <label class=" form-control-label">Tên khóa học</label>
-                                                    <input type="text" placeholder="Tên khóa học" required name="name" class="form-control">
-                                                </div>
-
-                                                <div class="form-group">
-                                                    <label class=" form-control-label">Mô tả</label>
-                                                    <textarea name="description" placeholder="Mô tả khóa học..." class="form-control" rows="6"></textarea>
-                                                </div>
-
-                                                <div class="form-group row justify-content-center">
-                                                    <button type="submit" name="add" class="btn btn-info btn-lg">
-                                                        <i class="ti-check"></i> Thêm
-                                                    </button>
-                                                    <button type="reset" class="btn btn-outline-secondary btn-lg ml-2">
-                                                        <i class="ti-back-left"></i> Nhập lại
-                                                    </button>
-                                                </div>
-
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
 
                                 <div class="col-lg-12 mt-2">
                                     <div class="card">
@@ -103,10 +75,10 @@
                                                             <td><?php echo $row['name']; ?></td>
                                                             <td>
                                                                 <a href="./course-edit.php?course=<?php echo $row['id']; ?>" class="btn btn-warning btn-md test">
-                                                                    <i class="ti-pencil-alt"></i> Sửa
+                                                                    <i class="ti-pencil-alt"></i> Chỉnh sửa
                                                                 </a>
-                                                                <a href="./actions/course-delete.php?course=<?php echo $row['id']; ?>" onclick="delete_course(event)" class="btn btn-outline-danger btn-md">
-                                                                    <i class="ti-trash"></i> Xóa
+                                                                <a href="./actions/course-delete.php?course=<?php echo $row['id']; ?>" class="btn btn-outline-info btn-md">
+                                                                    <i class="ti-trash"></i> Tạo đề thi
                                                                 </a>
                                                             </td>
                                                         </tr>
@@ -139,59 +111,7 @@
 
     <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 
-
-
-    <!-- sweetAlert -->
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
     <script src="assets/js/main.js"></script>
-
-    <?php if (isset($_POST['add'])) {
-        include_once("../config/connection.php");
-        $query = "INSERT INTO khoa_hoc VALUES(NULL,'$_POST[name]','$_POST[description]')";
-        $res = mysqli_query($connect, $query);
-        if ($res) {
-    ?>
-            <script>
-                swal({
-                    title: "Thành công!",
-                    text: "Đã thêm khóa học mới!",
-                    icon: "success",
-                    button: "Đóng",
-                }).then((value) => {
-                    window.location.href = window.location.href;
-                })
-            </script>
-
-    <?php }
-        $link->close_connect();
-    } ?>
-
-
-    <!-- delete by sweetAlert -->
-    <script>
-        function delete_course(event) {
-            event.preventDefault();
-            let link_redirect = event.currentTarget.getAttribute('href');
-            // alert('clicked: '+link_redirect)
-            swal({
-                    title: "Bạn có chắc chắn muốn xóa khóa học này?",
-                    text: "Chú ý: hành động không thể hoàn tác!",
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                })
-                .then((willDelete) => {
-                    if (willDelete) {
-                        swal("Xóa thành công", {
-                            icon: "success",
-                        }).then((value) => {
-                            window.location.href = link_redirect;
-                        });
-                    }
-                });
-        }
-    </script>
 
 </body>
 
